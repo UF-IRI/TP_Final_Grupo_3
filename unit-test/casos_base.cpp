7d17f296e5549ac0a5c890bcbfc638f4c8e29a89
Valentina Mao <maovalentina2004@gmail.com>
2022-11-17 22:45:23 -0300

    compu valen

--- unit-test/casos_base.cpp
+++ unit-test/casos_base.cpp
@@ -1,7 +1,6 @@
 #include "gmock/gmock.h"
 #include "headers.h"
-#include <iostream>
-#include <fstream>
+
 
 
 using namespace testing;
@@ -30,101 +29,145 @@ TEST(archivos, abrirlos) {
 		}
 
 	}
-	//TEST(archivosconsul, abrirlos)
-	//{
-	//	bool abierto;
-	//	fstream prueba;
-	//	consults Consultprueba;// 867584849"01 / 11 / 2001", "19 / 12 / 2001", true, "00 - 334 - 0838"
-	//
-	//	Consultprueba.ID = 867584849;
-	//	prueba.open("archivoprueba.csv", ios::out);
-	//	if (!(prueba.is_open()))
-	//		abierto = false;
-	//	else
-	//	{
-	//		abierto = true;
-	//		prueba << "dni_pac, fecha_solicitado, fecha_turno, presento, matricula_med";
-	//		prueba << Consultprueba.ID << "," << Consultprueba.required << "," << Consultprueba.appointment << "," << Consultprueba.attendance << "," << Consultprueba.doctors_ID;
-	//		prueba.close();
-	//		int Nconsults = 0;
-	//		consults* list_consults = new consults[Nconsults];
+	TEST(archivosconsul, abrirlos)
+	{
+		bool abierto;
+		fstream prueba;
+		consults Consultprueba;// 867584849"01 / 11 / 2001", "19 / 12 / 2001", true, "00 - 334 - 0838"
+	
+		Consultprueba.ID = 867584849;
+		prueba.open("archivoprueba.csv", ios::out);
+		if (!(prueba.is_open()))
+			abierto = false;
+		else
+		{
+			abierto = true;
+			prueba << "dni_pac, fecha_solicitado, fecha_turno, presento, matricula_med";
+			prueba << Consultprueba.ID << "," << Consultprueba.required << "," << Consultprueba.appointment << "," << Consultprueba.attendance << "," << Consultprueba.doctors_ID;
+			prueba.close();
+			int Nconsults = 0;
+			consults* list_consults = new consults[Nconsults];
 
-	//		bool test = readConsults("archivoprueba.csv", *&list_consults, &Nconsults);
-	//		ASSERT_THAT(Nconsults, 1);
-	//		EXPECT_TRUE(test);
-	//		ASSERT_THAT(list_consults[0].ID, Consultprueba.ID);
-	//	}
-	//}
-	//TEST(archivosContact, abrirlos)
-	//{
-	//	bool abierto;
-	//	fstream prueba;
-	//	contacts Contactprueba = { 967597385," + 41 203 339 0504"," + 57 722 907 7201","Pine View","strillow0@is.gd" };
+			bool test = readConsults("archivoprueba.csv", *&list_consults, &Nconsults);
+			ASSERT_THAT(Nconsults, 1);
+			EXPECT_TRUE(test);
+			ASSERT_THAT(list_consults[0].ID, Consultprueba.ID);
+		}
+	}
+	TEST(archivosContact, abrirlos)
+	{
+		bool abierto;
+		fstream prueba;
+		contacts Contactprueba = { 967597385," + 41 203 339 0504"," + 57 722 907 7201","Pine View","strillow0@is.gd" };
 
-	//	prueba.open("archivoprueba.csv", ios::out);
-	//	if (!(prueba.is_open()))
-	//		abierto = false;
-	//	else
-	//	{
-	//		abierto = true;
-	//		prueba << "dni_pac,telefono,celular,direccion,mail";
-	//		prueba << Contactprueba.ID << "," << Contactprueba.telephone << "," << Contactprueba.celphone << "," << Contactprueba.direccion << "," << Contactprueba.mail;
-	//		prueba.close();
-	//		int Ncontacts = 0;
-	//		contacts* list_contacts = new contacts[Ncontacts];
+		prueba.open("archivoprueba.csv", ios::out);
+		if (!(prueba.is_open()))
+			abierto = false;
+		else
+		{
+			abierto = true;
+			prueba << "dni_pac,telefono,celular,direccion,mail";
+			prueba << Contactprueba.ID << "," << Contactprueba.telephone << "," << Contactprueba.celphone << "," << Contactprueba.direccion << "," << Contactprueba.mail;
+			prueba.close();
+			int Ncontacts = 0;
+			contacts* list_contacts = new contacts[Ncontacts];
 
-	//		bool test = readContact("archivoprueba.csv", *&list_contacts, &Ncontacts);
-	//		ASSERT_THAT(Ncontacts, 1);
-	//		EXPECT_TRUE(test);
-	//		EXPECT_THAT(list_contacts[0].ID, Contactprueba.ID);
-	//	}
-	//}
-	//TEST(archivosObraSoc, abrirlos)
-	//{
-	//	bool abierto;
-	//	fstream prueba;
-	//	insurance Insuranceprueba = { 1,"Medicus"};
+			bool test = readContact("archivoprueba.csv", *&list_contacts, &Ncontacts);
+			ASSERT_THAT(Ncontacts, 1);
+			EXPECT_TRUE(test);
+			EXPECT_THAT(list_contacts[0].ID, Contactprueba.ID);
+		}
+	}
+	TEST(archivosObraSoc, abrirlos)
+	{
+		bool abierto;
+		fstream prueba;
+		insurance Insuranceprueba = { 1,"Medicus"};
 
-	//	prueba.open("archivoprueba.csv", ios::out);
-	//	if (!(prueba.is_open()))
-	//		abierto = false;
-	//	else
-	//	{
-	//		abierto = true;
-	//		prueba << "id_os,obra_social";
-	//		prueba << Insuranceprueba.ID << "," << Insuranceprueba.insurance_name;
-	//		prueba.close();
-	//		int Ninsurances = 0;
-	//		insurance* list_insurances = new insurance[Ninsurances];
-	//		bool test = readInsurances("archivoprueba.csv", *&list_insurances, &Ninsurances);
-	//		ASSERT_THAT(Ninsurances, 1);
-	//		EXPECT_TRUE(test);
-	//		EXPECT_THAT(list_insurances[0].ID, Insuranceprueba.ID);
-	//	}
-	//	EXPECT_TRUE(abierto);
-	//}
-	//TEST(archivosMed, abrirlos)
-	//{
-	//	bool abierto;
-	//	fstream prueba;
-	//	doctor Doctorprueba = { "04 - 723 - 0907","Araldo","Grafom","827 - 190 - 3137","farmacologia clinica",true};
+		prueba.open("archivoprueba.csv", ios::out);
+		if (!(prueba.is_open()))
+			abierto = false;
+		else
+		{
+			abierto = true;
+			prueba << "id_os,obra_social";
+			prueba << Insuranceprueba.ID << "," << Insuranceprueba.insurance_name;
+			prueba.close();
+			int Ninsurances = 0;
+			insurance* list_insurances = new insurance[Ninsurances];
+			bool test = readInsurances("archivoprueba.csv", *&list_insurances, &Ninsurances);
+			ASSERT_THAT(Ninsurances, 1);
+			EXPECT_TRUE(test);
+			EXPECT_THAT(list_insurances[0].ID, Insuranceprueba.ID);
+		}
+		EXPECT_TRUE(abierto);
+	}
+	TEST(archivosMed, abrirlos)
+	{
+		bool abierto;
+		fstream prueba;
+		doctor Doctorprueba = { "04 - 723 - 0907","Araldo","Grafom","827 - 190 - 3137","farmacologia clinica",true};
 
-	//	prueba.open("archivoprueba.csv", ios::out);
-	//	if (!(prueba.is_open()))
-	//		abierto = false;
-	//	else
-	//	{
-	//		abierto = true;
-	//		prueba << "matricula,nombre,apellido,telefono,especialidad,activo";
-	//		prueba << Doctorprueba.ID << "," << Doctorprueba.name<<","<< Doctorprueba.surname<<","<< Doctorprueba.telephone<<","<< Doctorprueba.speciality<<","<< Doctorprueba.state;
-	//		prueba.close();
-	//		int Ndoctors = 0;
-	//		doctor* list_doctors = new doctor[Ndoctors];
-	//		bool test = readDoctor("archivoprueba.csv", *&list_doctors, &Ndoctors);
-	//		ASSERT_THAT(Ndoctors, 1);
-	//		EXPECT_TRUE(test);
-	//		EXPECT_THAT(list_doctors[0].ID, Doctorprueba.ID);
-	//	}
-	//	EXPECT_TRUE(abierto);
-	//}
-	//FALTA TEST DE LA BUSQUEDA, LAS FUNCIONES DEL TIEMPO Y VER LO DE ARCHIVADOS Y RECUPERABLES
+		prueba.open("archivoprueba.csv", ios::out);
+		if (!(prueba.is_open()))
+			abierto = false;
+		else
+		{
+			abierto = true;
+			prueba << "matricula,nombre,apellido,telefono,especialidad,activo";
+			prueba << Doctorprueba.ID << "," << Doctorprueba.name<<","<< Doctorprueba.surname<<","<< Doctorprueba.telephone<<","<< Doctorprueba.speciality<<","<< Doctorprueba.state;
+			prueba.close();
+			int Ndoctors = 0;
+			doctor* list_doctors = new doctor[Ndoctors];
+			bool test = readDoctor("archivoprueba.csv", *&list_doctors, &Ndoctors);
+			ASSERT_THAT(Ndoctors, 1);
+			EXPECT_TRUE(test);
+			EXPECT_THAT(list_doctors[0].ID, Doctorprueba.ID);
+		}
+		EXPECT_TRUE(abierto);
+	}
+	//bool search(patient*& list_patient, int* Npatient, consults*& list_consults, int* Nconsults, contacts*& list_contacts, int* Ncontacts)
+	
+	TEST(search, noConincidence)
+	{
+		int Np = 2;
+		patient* list_patient = new patient[Np];
+		list_patient[0]= {45,"valen","mao",'F',"07/06/2004","n/c","italiano"};
+		list_patient[1] = { 73,"guada","helou",'F',"24/03/2004","n/c","medicus" };
+		int Nc = 4;
+		consults* list_consults = new consults[Nc];
+		list_consults[0] = { 45,"28/09/2017","28/09/2018",false,"218976" };
+		list_consults[1] = { 45,"28/09/2015","28/09/2016",true,"218976" };
+		list_consults[2] = { 73,"28/09/2014","28/09/2015",false,"218976" };
+		list_consults[3] = { 73,"28/09/2015","28/09/2016",true,"218976" };
+		int Ncons = 3;
+		contacts* list_contacts = new contacts[Ncons];
+		int ID;
+		list_contacts[0] = { 47586,"02304666144","+5491122832779","vuelta de obligado 875","lupehelou@gmail.com" };
+		list_contacts[1] = { 98765,"6482945659","+5491112345678","av santa fe 4576","glopez@gmail.com" };
+		list_contacts[2] = { 45678,"02846892","+5491198765432","jose hernandez 3412","dperez@gmail.com" };
+
+		bool check = search(list_patient, &Np, list_consults, &Nc, list_contacts, &Nc);
+
+
+
+		int ID;
+		string required;
+		string appointment;
+		bool attendance;
+		string doctors_ID;
+
+
+	}
+
+	TEST(fromStringToTimet, coincide)
+	{
+		string aux = "21/03/2004";
+		tm auxiliar{};
+		auxiliar.tm_mday = 21;
+		auxiliar.tm_mon = 03-1;
+		auxiliar.tm_year = 2004-1900;
+		time_t esperado= mktime(&auxiliar);
+		time_t real = fromStringtoTimet(aux);
+		EXPECT_THAT(esperado, real);
+	}
