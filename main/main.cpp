06d1050412894ecc5cc82145e5a13cca53c863a9
Valentina Mao <maovalentina2004@gmail.com>
2022-11-17 23:20:26 -0300

    valex2

--- main/main.cpp
+++ main/main.cpp
@@ -2,28 +2,46 @@
 #include "headers.h"
 
 
-int main() 
+int main()
 {
+	//genero las variables de pacientes y leo el archivo
 	int Npatient = 0;
-	patient *list_patient = new patient[Npatient];//cuando hagas esto ejecuta paso po paso, esta buscando el archivo en tu compu y no en la carpeta esta de porqueria
+	patient* list_patient = new patient[Npatient];//cuando hagas esto ejecuta paso po paso, esta buscando el archivo en tu compu y no en la carpeta esta de porqueria
 	string patientPath = "IRI_Pacientes.csv";//SUERTE<3
+	bool checkPatient = readpatient(patientPath, list_patient, &Npatient);
+
+	//genero las variables de contactos y leo el archivo
 	int Ncontacts = 0;
 	contacts* list_contacts = new contacts[Ncontacts];
+	string contactPath = "IRI_Contactos.csv";
+	bool checkContacts = readContact(contactPath, list_contacts, &Ncontacts);
+
+	//genero las variables de consultas y leo el archivo
 	int Nconsults = 0;
 	consults* list_consults = new consults[Nconsults];
-	string contactPath = "IRI_Contactos.csv";
 	string consultsPath = "IRI_Consultas.csv";
+	bool checkConsults = readConsults(consultsPath, list_consults, &Nconsults);
+
+	//genero las variables de medicos y leo el archivo	
 	string doctorsPath = "IRI_Medicos.csv";
 	int Ndoctors = 0;
 	doctor* list_docs = new doctor[Ndoctors];
 	bool checkDoc = readDoctor(doctorsPath, list_docs, &Ndoctors);
-	//bool check = readpatient(patientPath, list_patient, &Npatient);
-	//bool checkContacts = readContact(contactPath, list_contacts, &Ncontacts);
-	//bool check = readConsults(consultsPath, list_consults, &Nconsults);
+	
+	//genero las variables de obra social y leo el archivo
+	string insurancePath = "IRI_ObraSocial.csv";
+	int Nos = 0;
+	insurance* list_insurance = new insurance[Nos];
+	bool checkInsurance = readInsurances(insurancePath, list_insurance, &Nos);
+	
 
-	for (int i = 0; i < Ndoctors; i++)
-	{
-		cout <<"\n" << list_docs[i].speciality;
-	}
+	delete[]list_patient;
+	delete[]list_consults;
+	delete[]list_contacts;
+	delete[]list_docs;
+	delete[]list_insurance;
 	return 0;
+	
 }
+
+
